05a80d7e045c033677c529d63d8bf66ca84e10ba
Steven <steven.samuel@ui.ac.id>
2023-05-27 21:28:57 +0700

    coba baru

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,78 @@
-// @prepros-append vendor/jquery-3.3.1.min.js
-// @prepros-append vendor/popper.min.js
-// @prepros-append vendor/bootstrap.min.js
-// @prepros-append vendor/owl.carousel.min.js
-// @prepros-append vendor/aos.js
-// @prepros-append vendor/isotope.pkgd.min.js
-// @prepros-append vendor/jquery.animateNumber.min.js
-// @prepros-append vendor/jquery.waypoints.min.js
-// @prepros-append vendor/TweenMax.min.js
-// @prepros-append vendor/jquery.easing.1.3.js
-// @prepros-append vendor/jarallax.min.js
-// @prepros-append vendor/jarallax-video.min.js
-// @prepros-append vendor/jquery.validate.min.js
-// @prepros-append vendor/jquery.fancybox.min.js
-// @prepros-append vendor/stickyfill.min.js
-// @prepros-append vendor/imagesloaded.pkgd.min.js
-// @prepros-append vendor/ScrollMagic.min.js
-// @prepros-append vendor/scrollmagic.animation.gsap.min.js
+/* Description: Custom JS file */
+
+
+(function($) {
+    "use strict"; 
+	
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+    
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+    });
+
+    // offcanvas script from Bootstrap + added element to close menu on click in small viewport
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+    // hover in desktop mode
+    function toggleDropdown (e) {
+        const _d = $(e.target).closest('.dropdown'),
+            _m = $('.dropdown-menu', _d);
+        setTimeout(function(){
+            const shouldOpen = e.type !== 'click' && _d.is(':hover');
+            _m.toggleClass('show', shouldOpen);
+            _d.toggleClass('show', shouldOpen);
+            $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+        }, e.type === 'mouseleave' ? 300 : 0);
+    }
+    $('body')
+    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+    .on('click', '.dropdown-menu a', toggleDropdown);
+
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+	});
+	
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
