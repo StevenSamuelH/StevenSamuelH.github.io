8c647f81c449ec52e60ebf3dbe04b76c1e8b1778
Steven Samuel Hutapea <75583695+StevenSamuelH@users.noreply.github.com>
2024-01-14 00:32:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-
+ experimental: {
+    images: true, // Make sure this is a valid configuration option
+    // Other experimental options if any
+  },
 };
 
 module.exports = nextConfig
