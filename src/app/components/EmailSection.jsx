10db1a10bc62e484508d4ef8e6e3b40c45b4fe5e
Steven Samuel Hutapea <75583695+StevenSamuelH@users.noreply.github.com>
2024-01-14 00:53:51 +0700

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -4,6 +4,7 @@ import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
+
 const EmailSection = () => {
 
   return (
@@ -31,9 +32,7 @@ const EmailSection = () => {
           </Link>
         </div>
       </div>
-       
-           
-      </div>
+    
     </section>
   );
 };
