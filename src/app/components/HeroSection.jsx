dd53ee033e1c386df9fac2cfddbfd1c96d1b174f
Steven <steven.samuel@ui.ac.id>
2024-01-16 22:56:08 +0700

    baru

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,9 +17,10 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-600">
               Hello, I&apos;m{" "}
-            </span>
+          </span>
+
             <br></br>
             <TypeAnimation
               sequence={[
@@ -39,14 +40,15 @@ const HeroSection = () => {
           <Link href="https://www.linkedin.com/in/steven-samuel-hutapea-12b9b91b7/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
-            <Link
-              href="https://drive.google.com/uc?export=download&id=1dPLIXUwQTil7cIIeEbQ1QfPLbh77uICu"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-            >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </Link>
+          <Link
+  href="https://drive.google.com/uc?export=download&id=1dPLIXUwQTil7cIIeEbQ1QfPLbh77uICu"
+  className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-blue-700 hover:bg-slate-800 text-white mt-3"
+>
+  <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+    Download CV
+  </span>
+</Link>
+
           </div>
         </motion.div>
         <motion.div
